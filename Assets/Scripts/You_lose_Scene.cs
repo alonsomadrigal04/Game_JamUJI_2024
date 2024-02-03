a148f130d2c62bb4afcdf076ec0c51605cdf9305
Alonso Madrigal <alonso.madrigal.h@gmail.com>
2024-02-03 11:21:42 +0100

    Game_loop_1

---
+++ Assets/Scripts/You_lose_Scene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class You_lose_Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string sceneToLoad = "Alonso_Scene"; // Ingresa el nombre de la escena que deseas reiniciar
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
